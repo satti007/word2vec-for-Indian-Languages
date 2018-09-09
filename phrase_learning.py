dda1c188de3fa298fa0fbc82cbe7aff795142d81
satti007 <gsatishkumaryadav@gmail.com>
2018-09-09 20:42:25 +0530

    bigrams count in preprocessing

---
+++ phrase_learning.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+
+files_dir = '../../data/corpora/combined_stats/'						# base directory for frequency(unigram & bigram) files
+
+vocab      = pd.read_csv(files_dir +'word_to_freq.csv',header=None)
+bigrams    = pd.read_csv(files_dir +'bigram_to_freq.csv',header=None)
+phrases_doc    = open(files_dir+'phrases.txt','w')
+word_to_freq   = dict(zip(list(vocab[0])  ,list(vocab[1])))					# dict - {word:count(word)}
+bigram_to_freq = dict(zip(list(bigrams[0]),list(bigrams[1])))				# dict - {(w1,w2):count(w1,w2)}
+tot_tokens     = sum(word_to_freq.values()) 								# total number of tokens
+
+passes     = 1 				# No.of iterations over the training data to allow longer phrases formation
+discount   = 5				# discard unigram & bigrams that appear < discount
+threshold  = 50				# threshold for forming the phrases
+decrement  = 10				# decrease threshold value by decrement after each pass to allow longer phrases formation
+
+# check if unigrams(w1&w2) & bigrams appear < discount
+def OVV(bigram,w1,w2):
+	if bigram_to_freq[bigram]  < discount:
+		return 1
+	if word_to_freq[w1] < discount:
+		return 1
+	if word_to_freq[w2] < discount:
+		return 1
+	
+	return 0
+
+# caluclate score used for phrasing
+def calScore(bigram,w1,w2):
+	print ('bigram: ',bigram_to_freq[bigram])
+	print ('w1: ',word_to_freq[w1])
+	print ('w2: ',word_to_freq[w2])
+	return ((float)(bigram_to_freq[bigram]/word_to_freq[w1])/word_to_freq[w2])*tot_tokens
+
+# check if the the word is a int/float
+def checkNum(word):
+	W = word.replace('"','').replace(',','').replace('.','') 	# remove '"' ',' '.'  
+	try:
+		x = float(W)
+		return 1 
+	except ValueError:
+		return 0
+
+iter  = 0
+bigram_count = 0
+phrase_count = 0
+while (iter < passes):
+	print('Starting iteration {} with threshold {}...'.format(iter,threshold))
+	for bigram in bigram_to_freq:
+		bigram_count = bigram_count + 1
+		if bigram_count%100000 == 0:
+			print('bigrams_count: {}, phrase_count: {}'.format(bigram_count,phrase_count)) 
+			break
+		w1, w2 = bigram[1:-1].split(',')[0].strip()[1:-1],bigram[1:-1].split(',')[-1].strip()[1:-1] 
+		if len(w1) and len(w2):				# ** -- some probs due to ','
+			print('bigram:{}, w1:{}, w2:{}'.format(bigram,w1,w2))
+			if checkNum(w1):
+				continue
+			if checkNum(w2):
+				continue
+			if OVV(bigram,w1,w2):
+				continue
+			score = calScore(bigram,w1,w2)
+			print (score)
+			if score < threshold:
+				continue
+			phrase = w1 + '_' + w2 + '\n'
+			phrases_doc.write(phrase)
+			phrase_count = phrase_count + 1
+	print('End of iteration {} with threshold {}'.format(iter,threshold))
+	iter = iter+ 1
+	threshold = threshold - decrement
+
+phrases_doc.close()
--- preprocessing.py
+++ preprocessing.py
@@ -9,7 +9,7 @@ from polyglot.text import Text
 wiki_dir = '../../data/corpora/wiki/'						# base directory for wiki articles
 AJ_dir   = '../../data/corpora/andhrajyothy/'				# base directory for andhrajyothy articles
 
-files_dir =  wiki_dir 										# AJ_dir (or) wiki_dir
+files_dir =  AJ_dir 										# AJ_dir (or) wiki_dir
 
 folders   = sorted(os.listdir(files_dir+'articles/'))		# folders with articles
 word_doc  = open(files_dir+'stats/vocab.txt','wb',0)		# txt file to write vocabulary
@@ -77,55 +77,65 @@ def update_vocab_count(nums,words,sent,word_to_freq,hash_to_line,uni_sen):
 	
 	return word_to_freq,hash_to_line,uni_sen
 
+# bigram_count
 def get_bigran_count(words,bigram_to_freq):
 	for i in range(0,len(words)-1):
 		w1,w2 = words[i],words[i+1]
-		if len(re_tel(w1)) or len(re_tel(w2)):
-			if [w1,w2] not in bigram_to_freq:
-				bigram_doc.write((w+'\n').encode('utf-8'))
-				bigram_to_freq[[w1,w2]]  = 1
+		if len(re_tel.sub(r'',w1)) or len(re_tel.sub(r'',w2)):
+			if (w1,w2) not in bigram_to_freq:
+				bigram_to_freq[(w1,w2)] = 1
 			else:
-				bigram_to_freq[[w1,w2]]  = 1
+				bigram_to_freq[(w1,w2)] += 1
+	
+	return bigram_to_freq
 
-# save dicts (freq_count, hash_to_line -- useful if you are preprocessing the data in chunks)
-def save_dicts(word_to_freq,hash_to_line):
-	word_to_freq = dict(sorted(word_to_freq.items(),key=lambda x:x[1],reverse=True))
+# delete bigrams with count=1 
+def clrBigrams_1(bigram_to_freq):
+	return {k:v for k,v in bigram_to_freq.items() if v>1}
+
+# save dicts (freq_count,bigram_count,hash_to_line -- useful if you are preprocessing the data in chunks)
+def save_dicts(word_to_freq,bigram_to_freq,hash_to_line):
+	word_to_freq   = dict(sorted(word_to_freq.items()  ,key=lambda x:x[1],reverse=True))
+	bigram_to_freq = dict(sorted(bigram_to_freq.items(),key=lambda x:x[1],reverse=True))
 	pd.DataFrame.from_dict(data=word_to_freq,orient='index').to_csv(files_dir+'stats/word_to_freq.csv',header=None)
 	pd.DataFrame.from_dict(data=hash_to_line,orient='index').to_csv(files_dir+'stats/hash_to_line.csv',header=None)
+	pd.DataFrame.from_dict(data=bigram_to_freq,orient='index').to_csv(files_dir+'stats/bigram_to_freq.csv',header=None)
 
 # write stats to a file
 def save_stats(stats):
 	file = open(files_dir+'stats/stats.txt','w')
 	file.write('Number of sentences:           {}M\n'.format(round(stats[0]/pow(10.0,6),2)))
 	file.write('Number of unique sentences:    {}M\n'.format(round(stats[1]/pow(10.0,6),2)))
-	file.write('Number of words    in vocabulary: {}M\n'.format(round(stats[2]/pow(10.0,6),2)))
-	file.write('Number of sp.chars in vocabulary: {}\n'.format(len(stats[3])))
-	file.write('Number of tokens    in corpora:   {}M\n'.format(round(stats[4]/pow(10.0,6),2)))
-	file.write('Number of sp.tokens in corpora:   {}M\n'.format(round(stats[5]/pow(10.0,6),2)))
-	file.write('Number of words with freq>=5 in vocabulary:  {}K\n'.format(round(stats[6]/pow(10.0,3),2)))
-	file.write('Number of words with freq>=10 in vocabulary: {}K\n'.format(round(stats[7]/pow(10.0,3),2)))
-	file.write('Number of words with freq>=20 in vocabulary: {}K\n'.format(round(stats[8]/pow(10.0,3),2)))
-	file.write('\n\nsp.chars = sp.tokens:\n{}'.format(stats[3]))
+	file.write('Number of words     in vocabulary: {}M\n'.format(round(stats[2]/pow(10.0,6),2)))
+	file.write('Number of sp.chars  in vocabulary: {}\n'.format(len(stats[4])))
+	file.write('Number of bigrams   in corpora   : {}M\n'.format(round(stats[3]/pow(10.0,6),2)))
+	file.write('Number of tokens    in corpora:   {}M\n'.format(round(stats[5]/pow(10.0,6),2)))
+	file.write('Number of sp.tokens in corpora:   {}M\n'.format(round(stats[6]/pow(10.0,6),2)))
+	file.write('Number of words with freq>=5 in vocabulary:  {}K\n'.format(round(stats[7]/pow(10.0,3),2)))
+	file.write('Number of words with freq>=10 in vocabulary: {}K\n'.format(round(stats[8]/pow(10.0,3),2)))
+	file.write('Number of words with freq>=20 in vocabulary: {}K\n'.format(round(stats[9]/pow(10.0,3),2)))
+	file.write('\n\nsp.chars = sp.tokens:\n{}'.format(stats[4]))
 	file.close()
 
 # caluclate the stats
-def get_stats(word_to_freq,tot_sen,uni_sen):
+def get_stats(word_to_freq,bigram_to_freq,tot_sen,uni_sen):
 	word_to_freq = dict(sorted(word_to_freq.items(),key=lambda x:x[1],reverse=True))
-	tot_vocab  = len(word_to_freq)
-	pun_vocab  = [w for w in punctuation if w in word_to_freq]
-	tot_tokens = sum(word_to_freq.values())
-	pun_tokens = sum(word_to_freq[w] for w in punctuation if w in word_to_freq)
-	vocab_5    = sum(1 for f in word_to_freq.values() if f >= 5)
-	vocab_10   = sum(1 for f in word_to_freq.values() if f >= 10)
-	vocab_20   = sum(1 for f in word_to_freq.values() if f >= 20)
+	tot_vocab    = len(word_to_freq)
+	bigram_count = len(bigram_to_freq)
+	pun_vocab    = [w for w in punctuation if w in word_to_freq]
+	tot_tokens   = sum(word_to_freq.values())
+	pun_tokens   = sum(word_to_freq[w] for w in punctuation if w in word_to_freq)
+	vocab_5      = sum(1 for f in word_to_freq.values() if f >= 5)
+	vocab_10     = sum(1 for f in word_to_freq.values() if f >= 10)
+	vocab_20     = sum(1 for f in word_to_freq.values() if f >= 20)
 	
-	save_stats([tot_sen,uni_sen,tot_vocab,pun_vocab,tot_tokens,pun_tokens,vocab_5,vocab_10,vocab_20])
+	save_stats([tot_sen,uni_sen,tot_vocab,bigram_count,pun_vocab,tot_tokens,pun_tokens,vocab_5,vocab_10,vocab_20])
 
 tot_sen 	 = 0
 uni_sen 	 = 0
 file_count   = 0
-word_to_freq = {}
-hash_to_line = {}
+word_to_freq   = {}
+hash_to_line   = {}
 bigram_to_freq = {}
 
 for F in folders:
@@ -134,6 +144,11 @@ for F in folders:
 		text = open(files_dir+'articles/'+F+'/'+f,'r').read()
 		text = re_valid.sub(r'',text)							# keep only {telugu chars, number, puntucation-marks}
 		text = Text(text)
+		try:
+			txt_words = text.words
+		except:
+			continue
+		bigram_to_freq = get_bigran_count(txt_words,bigram_to_freq)
 		try:
 			sentences = text.sentences
 		except:
@@ -157,10 +172,13 @@ for F in folders:
 				words = list(set(words)-set(nums))
 				word_to_freq,hash_to_line,uni_sen = update_vocab_count(nums,words,sent,word_to_freq,hash_to_line,uni_sen)
 		file_count += 1
-		print ('{}/{} done, file_count:{}, size of vocab: {}, tot_sen: {}, uni_sen: {}'
-							.format(F,f,file_count,len(word_to_freq),tot_sen,uni_sen))
+		print ('{}/{} done, file_count:{}, size of vocab: {}, size of bigrams: {}, tot_sen: {}, uni_sen: {}'
+							.format(F,f,file_count,len(word_to_freq),len(bigram_to_freq),tot_sen,uni_sen))
 		if file_count%10000 == 0:
-			save_dicts(word_to_freq,hash_to_line)
+			save_dicts(word_to_freq,bigram_to_freq,hash_to_line)
+		if file_count%100000 == 0:
+			bigram_to_freq = clrBigrams_1(bigram_to_freq)
 
-save_dicts(word_to_freq,hash_to_line)
-get_stats(word_to_freq,tot_sen,uni_sen)
+save_dicts(word_to_freq,bigram_to_freq,hash_to_line)
+bigram_to_freq = clrBigrams_1(bigram_to_freq)
+get_stats(word_to_freq,bigram_to_freq,tot_sen,uni_sen)
