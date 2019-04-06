1c85ab727e3ab4bfa84c5274cc0ccc1f7f388fa6
Satish G <gsatishkumaryadav@gmail.com>
2019-04-06 16:00:02 +0530

    Delete fastext.sh

--- helper_files/fastext.sh
+++
@@ -1,5 +0,0 @@
-./fasttext skipgram -input ../stats/sentences.txt\
-					 -output ../embds\
-					 -verbose 2 -minCount 5 -wordNgrams 14\
-					 -bucket 200000 -minn 3 -maxn 6 -t 1e-5\
-					 -lr 0.05 -dim 300 -ws 5 -epoch 10 -neg 10 -loss ns -thread 8
